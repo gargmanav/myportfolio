98cd5c975d4f80da8e3e7311d8349eb5235b0621
gargmanav <manavgarg1998@gmail.com>
2023-04-15 16:30:09 +0530

    last commit

--- portfoliomanav/src/pages/Resume.jsx
+++ portfoliomanav/src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/myresume.pdf`
+const resumeLink = `https://github.com/gargmanav/myportfolio/blob/main/portfoliomanav/src/assets/myresume.pdf`
 
 
 const Resume = () => {
